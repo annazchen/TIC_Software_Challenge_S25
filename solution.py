52baf616da55e87cc9acf3bf04a6bc76fa4db531
spiltmilkis <azchen@uwaterloo.ca>
2025-05-24 12:21:41 -0400

    attempt 3 lvl 1

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ import time
 from ultralytics import YOLO
 
 # Variable for controlling which level of the challenge to test -- set to 0 for pure keyboard control
-challengeLevel = 0
+challengeLevel = 1
 
 # Set to True if you want to run the simulation, False if you want to run on the real robot
 is_SIM = True
@@ -25,19 +25,21 @@ logging = Logging(robot)
 lidar = Lidar(robot)
 
 
-#debug
-if Debug:
-    lidar.checkScan()
-
 
 #functions
 def ifcollide():
     a = lidar.checkScan()
-    if lidar.detect_obstacle_in_cone(a, 0.1, 0, 45) != (-1,-1):
+    if Debug:
+        print("running lvl 1...")
+        print(lidar.detect_obstacle_in_cone(a, 0.1, 0, 30))
+
+    if lidar.detect_obstacle_in_cone(a, 0.4, 0, 45) != (-1,-1):
+        print("collide detected")
         control.stop_keyboard_control()
-        time.sleep(0.1)
-        control.move_backward()
-        time.sleep(1.5)
+        while(lidar.detect_obstacle_in_cone(a, 0.5,0,45)!=(-1,-1)):
+            control.move_backward()
+
+        time.sleep(0.5)
         control.start_keyboard_control()
     
 #define challenge level    
@@ -56,7 +58,7 @@ try:
     if challengeLevel == 1:
         while rclpy.ok():
             rclpy.spin_once(robot, timeout_sec=0.1)
-            time.sleep(0.1)
+            time.sleep(0.01)
             # Write your solution here for challenge level 1
             # It is recommended you use functions for aspects of the challenge that will be resused in later challenges
             # For example, create a function that will detect if the robot is too close to a wall
